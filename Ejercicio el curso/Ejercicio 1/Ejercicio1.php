6af92103c0cc71a84c86d423317afc7173ecf545
abayasb <79287819+abayasb@users.noreply.github.com>
2022-09-05 13:05:13 -0500

    Ejercicio

---
+++ Ejercicio el curso/Ejercicio 1/Ejercicio1.php
@@ -0,0 +1,19 @@
+<?php
+    /* 
+        Ejercicio 1: crear 2 variables
+        pais, continente, e imprimir el valor de las 
+        varibles por pantalla.
+        Poner en un comentario que tipo de datos tienen.
+    */
+
+    /* 
+        Primer creamos las variable pais y continente
+        EL tipo de datos que se utilizason es de tipo
+        String, tanto para la variable pais como con-
+        tinente
+    */
+    $pais = "Ecuador";
+    $continente = "America";
+    
+    echo "<h1>Pais: $pais esta en el continente $continente</h1>"
+?>
